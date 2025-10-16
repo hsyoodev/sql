0c9f96a655fd759e454fbaeb9e308ef24fced556
hsyoo <hsyoodev@icloud.com>
2025-10-16 23:55:25 +0900

    unsolve P151139_대여_횟수가_많은_자동차들의_월별_대여_횟수_구하기

---
+++ programmers/lv3/oracle/P151139_대여_횟수가_많은_자동차들의_월별_대여_횟수_구하기.sql
@@ -0,0 +1,27 @@
+SELECT
+        TO_CHAR(START_DATE, 'FMMM') AS MONTH
+      , CAR_ID
+      , COUNT(*) AS RECORDS
+FROM
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE
+        CAR_ID IN (
+                        SELECT
+                                CAR_ID
+                        FROM
+                                CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                        WHERE
+                                TO_CHAR(START_DATE, 'YYYY-MM-DD') < '2022-11-01'
+                        AND
+                                TO_CHAR(END_DATE, 'YYYY-MM-DD') >= '2022-08-01'
+                        GROUP BY
+                                CAR_ID
+                        HAVING
+                                COUNT(*) >= 5
+                  )
+GROUP BY
+        TO_CHAR(START_DATE, 'FMMM')
+      , CAR_ID
+ORDER BY
+        MONTH
+      , CAR_ID DESC;
