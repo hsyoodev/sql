11c93756d84192b538fbb83f5032daf3ecbb5382
hsyoo <hsyoodev@icloud.com>
2026-01-04 13:37:45 +0900

    retried P59045_보호소에서_중성화한_동물

--- programmers/lv4/P59045_보호소에서_중성화한_동물.sql
+++ programmers/lv4/P59045_보호소에서_중성화한_동물.sql
@@ -1,15 +1,11 @@
+-- 코드를 입력하세요
 SELECT
         A.ANIMAL_ID
       , A.ANIMAL_TYPE
       , A.NAME
 FROM
         ANIMAL_INS A
-      , ANIMAL_OUTS B
-WHERE
-        A.ANIMAL_ID = B.ANIMAL_ID
-AND
-        A.SEX_UPON_INTAKE LIKE 'Intact%'
-AND
-        REGEXP_LIKE(B.SEX_UPON_OUTCOME, 'Neutered|Spayed')
+INNER JOIN
+        ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID AND A.SEX_UPON_INTAKE LIKE 'Intact%' AND REGEXP_LIKE(B.SEX_UPON_OUTCOME, '^Spayed|Neutered')
 ORDER BY
-        A.ANIMAL_ID;
+    ANIMAL_ID;
