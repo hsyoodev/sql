8e26c74bbc371c31e74733275f813cd925edb6d9
hsyoo <hsyoodev@icloud.com>
2026-01-06 23:45:40 +0900

    retried P132204_취소되지_않은_진료_예약_조회하기

--- programmers/lv4/P132204_취소되지_않은_진료_예약_조회하기.sql
+++ programmers/lv4/P132204_취소되지_않은_진료_예약_조회하기.sql
@@ -1,14 +1,23 @@
+-- 코드를 입력하세요
 SELECT
         A.APNT_NO
-      , (SELECT PT_NAME FROM PATIENT WHERE PT_NO = A.PT_NO) AS PT_NAME
+      , B.PT_NAME
       , A.PT_NO
       , A.MCDP_CD
-      , (SELECT DR_NAME FROM DOCTOR WHERE DR_ID = A.MDDR_ID) AS DR_NAME
+      , C.DR_NAME
       , A.APNT_YMD
 FROM
         APPOINTMENT A
+INNER JOIN
+        PATIENT B
+        ON A.PT_NO = B.PT_NO
+INNER JOIN
+        DOCTOR C
+        ON A.MDDR_ID = C.DR_ID
 WHERE
         TO_CHAR(A.APNT_YMD, 'YYYY-MM-DD') = '2022-04-13'
+AND
+        A.MCDP_CD = 'CS'
 AND
         A.APNT_CNCL_YN = 'N'
 ORDER BY
