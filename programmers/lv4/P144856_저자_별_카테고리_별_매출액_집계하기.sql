8584ce8d13d9fc040b17e474e27ab38ae595bc81
hsyoo <hsyoodev@icloud.com>
2026-01-08 23:26:32 +0900

    retried P144856_저자_별_카테고리_별_매출액_집계하기

--- programmers/lv4/P144856_저자_별_카테고리_별_매출액_집계하기.sql
+++ programmers/lv4/P144856_저자_별_카테고리_별_매출액_집계하기.sql
@@ -1,26 +1,20 @@
+-- 코드를 입력하세요
 SELECT
-        C.AUTHOR_ID
-      , C.AUTHOR_NAME
-      , B.CATEGORY
-      , SUM(A.SALES * B.PRICE) AS TOTAL_SALES
+        AUTHOR_ID
+      , AUTHOR_NAME
+      , CATEGORY
+      , SUM(SALES * PRICE) AS TOTAL_SALES
 FROM
-        (
-            SELECT
-                    BOOK_ID
-                  , TO_CHAR(SALES_DATE, 'YYYY-MM') AS SALES_DATE
-                  , SALES
-            FROM
-                    BOOK_SALES
-        ) A
-      , BOOK B
-      , AUTHOR C
+        BOOK_SALES
+INNER JOIN
+        BOOK
+        USING(BOOK_ID)
+INNER JOIN
+        AUTHOR
+        USING(AUTHOR_ID)
 WHERE
-        A.BOOK_ID = B.BOOK_ID
-AND
-        B.AUTHOR_ID = C.AUTHOR_ID
-AND
-        A.SALES_DATE = '2022-01'
+        TO_CHAR(SALES_DATE, 'YYYY-MM') = '2022-01'
 GROUP BY
-        C.AUTHOR_ID, C.AUTHOR_NAME, B.CATEGORY
+        AUTHOR_ID, AUTHOR_NAME, CATEGORY
 ORDER BY
         AUTHOR_ID, CATEGORY DESC;
