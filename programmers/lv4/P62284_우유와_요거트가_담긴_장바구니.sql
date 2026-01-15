42e88ba4bec2a107cd5a04a06b28e40a236d8f63
hsyoo <hsyoodev@icloud.com>
2026-01-15 20:44:57 +0900

    retried P62284_우유와_요거트가_담긴_장바구니

--- programmers/lv4/P62284_우유와_요거트가_담긴_장바구니.sql
+++ programmers/lv4/P62284_우유와_요거트가_담긴_장바구니.sql
@@ -1,14 +1,15 @@
+-- 코드를 입력하세요
 SELECT
         CART_ID
 FROM
-        (
-            SELECT
-                    DISTINCT CART_ID, NAME
-            FROM
-                    CART_PRODUCTS
-            WHERE
-                    NAME IN ('Milk', 'Yogurt')
-        )
+    (
+        SELECT
+                DISTINCT CART_ID, NAME
+        FROM
+                CART_PRODUCTS
+        WHERE
+                NAME IN ('Milk', 'Yogurt')
+    )
 GROUP BY
         CART_ID
 HAVING
