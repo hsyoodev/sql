9ced6620e82f4eac9378d37732e0cc1487bd3ea3
hsyoo <hsyoodev@icloud.com>
2026-01-16 21:57:29 +0900

    retried P131124_그룹별_조건에_맞는_식당_목록_출력하기

--- programmers/lv4/P131124_그룹별_조건에_맞는_식당_목록_출력하기.sql
+++ programmers/lv4/P131124_그룹별_조건에_맞는_식당_목록_출력하기.sql
@@ -1,24 +1,28 @@
+-- 코드를 입력하세요
+WITH CUSTOM_REST_REVIEW AS
+ (
+    SELECT
+            MEMBER_ID
+    FROM
+            REST_REVIEW
+    GROUP BY
+            MEMBER_ID
+    ORDER BY
+            COUNT(*) DESC
+    FETCH
+            FIRST ROWS ONLY
+ )
 SELECT
-        B.MEMBER_NAME
-      , A.REVIEW_TEXT
-      , TO_CHAR(A.REVIEW_DATE, 'YYYY-MM-DD') AS REVIEW_DATE
+        MEMBER_NAME
+      , REVIEW_TEXT
+      , TO_CHAR(REVIEW_DATE, 'YYYY-MM-DD') AS REVIEW_DATE
 FROM
-        REST_REVIEW A
-      , MEMBER_PROFILE B
-WHERE
-        A.MEMBER_ID = B.MEMBER_ID
-AND
-        A.MEMBER_ID IN (
-                            SELECT
-                                    MEMBER_ID
-                            FROM
-                                    REST_REVIEW
-                            GROUP BY
-                                    MEMBER_ID
-                            ORDER BY
-                                    COUNT(*) DESC
-                            FETCH
-                                    FIRST 1 ROWS ONLY
-                       )
+        REST_REVIEW
+INNER JOIN
+        CUSTOM_REST_REVIEW
+        USING(MEMBER_ID)
+INNER JOIN
+        MEMBER_PROFILE
+        USING(MEMBER_ID)
 ORDER BY
         REVIEW_DATE, REVIEW_TEXT;
