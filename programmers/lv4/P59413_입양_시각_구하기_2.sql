c207ca1bfd4cd335020a07855ed32adc03fc0914
hsyoo <hsyoodev@icloud.com>
2026-01-21 23:30:10 +0900

    retried P59413_입양_시각_구하기_2

--- programmers/lv4/P59413_입양_시각_구하기_2.sql
+++ programmers/lv4/P59413_입양_시각_구하기_2.sql
@@ -1,23 +1,30 @@
+-- 코드를 입력하세요
+WITH
+    CUSTOM_ANIMAL_OUTS AS
+    (
+        SELECT
+                TO_CHAR(DATETIME, 'HH24') AS HOUR
+        FROM
+                ANIMAL_OUTS
+    )
+  , CUSTOM_HOUR AS
+    (
+        SELECT
+                LEVEL - 1 AS HOUR
+        FROM
+                DUAL
+        CONNECT BY
+                LEVEL <= 24
+    )
 SELECT
         A.HOUR
       , COUNT(B.HOUR) AS COUNT
 FROM
-        (
-            SELECT
-                    (LEVEL - 1) AS HOUR
-            FROM
-                    DUAL
-            CONNECT BY LEVEL <= 24
-        ) A
-      , (
-            SELECT
-                    TO_NUMBER(TO_CHAR(DATETIME, 'HH24')) AS HOUR
-            FROM
-                    ANIMAL_OUTS
-        ) B
-WHERE
-        A.HOUR = B.HOUR(+)
+        CUSTOM_HOUR A
+LEFT OUTER JOIN
+        CUSTOM_ANIMAL_OUTS B
+        ON A.HOUR = B.HOUR
 GROUP BY
         A.HOUR
 ORDER BY
-        HOUR;
+        A.HOUR;
