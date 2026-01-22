2a3a8139836a1bcd127e7e579893c08a9d2f456b
hsyoo <hsyoodev@icloud.com>
2026-01-22 22:03:00 +0900

    retried P90691_자동차_대여_기록_별_대여_금액_구하기

--- programmers/lv4/P90691_자동차_대여_기록_별_대여_금액_구하기.sql
+++ programmers/lv4/P90691_자동차_대여_기록_별_대여_금액_구하기.sql
@@ -1,27 +1,47 @@
+-- 코드를 입력하세요
+WITH
+    CUSTOM_CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS
+    (
+        SELECT
+                *
+        FROM
+                CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+        WHERE
+                CAR_TYPE = '트럭'
+    )
+  , CUSTOM_CAR_RENTAL_COMPANY_CAR AS
+    (
+        SELECT
+                *
+        FROM
+                CAR_RENTAL_COMPANY_CAR
+        WHERE
+                CAR_TYPE = '트럭'
+    )
+  , CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY AS
+    (
+        SELECT
+                HISTORY_ID
+              , CAR_ID
+              , (END_DATE - START_DATE + 1) AS DURATION_DATE
+              , CASE
+                    WHEN (END_DATE - START_DATE + 1) >= 90 THEN '90일 이상'
+                    WHEN (END_DATE - START_DATE + 1) >= 30 THEN '30일 이상'
+                    WHEN (END_DATE - START_DATE + 1) >= 7 THEN '7일 이상'
+                END AS DURATION_TYPE
+        FROM
+                CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    )
 SELECT
-        A.HISTORY_ID
-      , ROUND(A.DURATION * B.DAILY_FEE * (100 - NVL(C.DISCOUNT_RATE, 0)) / 100) AS FEE
+        HISTORY_ID
+      , (DURATION_DATE * DAILY_FEE) * (1 - NVL(DISCOUNT_RATE, 0) / 100) AS FEE
 FROM
-      (
-            SELECT
-                    HISTORY_ID
-                  , CAR_ID
-                  , (END_DATE - START_DATE + 1) AS DURATION
-                  , CASE 
-                        WHEN (END_DATE - START_DATE + 1) >= 90 THEN '90일 이상'
-                        WHEN (END_DATE - START_DATE + 1) >= 30 THEN '30일 이상'
-                        WHEN (END_DATE - START_DATE + 1) >= 7 THEN '7일 이상'
-                    END AS DURATION_TYPE
-            FROM
-                    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-      ) A
-INNER JOIN 
-        CAR_RENTAL_COMPANY_CAR B 
-ON 
-        A.CAR_ID = B.CAR_ID AND B.CAR_TYPE = '트럭'
-LEFT JOIN 
-        CAR_RENTAL_COMPANY_DISCOUNT_PLAN C 
-ON 
-        A.DURATION_TYPE = C.DURATION_TYPE AND C.CAR_TYPE = '트럭'
+        CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY
+INNER JOIN
+        CUSTOM_CAR_RENTAL_COMPANY_CAR
+        USING(CAR_ID)
+LEFT OUTER JOIN
+        CUSTOM_CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+        USING(DURATION_TYPE)
 ORDER BY
         FEE DESC, HISTORY_ID DESC;
