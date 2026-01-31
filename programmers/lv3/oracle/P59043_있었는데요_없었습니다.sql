66418db88234580764849ca29633a961b472195d
hsyoo <hsyoodev@icloud.com>
2026-01-31 23:02:11 +0900

    retried P59043_있었는데요_없었습니다

--- programmers/lv3/oracle/P59043_있었는데요_없었습니다.sql
+++ programmers/lv3/oracle/P59043_있었는데요_없었습니다.sql
@@ -1,12 +1,12 @@
+-- 코드를 입력하세요
 SELECT
         A.ANIMAL_ID
       , A.NAME
 FROM
         ANIMAL_INS A
-      , ANIMAL_OUTS B
-WHERE
-        A.ANIMAL_ID = B.ANIMAL_ID
-AND
-        (B.DATETIME - A.DATETIME) < 0
+INNER JOIN
+        ANIMAL_OUTS B
+        ON A.ANIMAL_ID = B.ANIMAL_ID
+        AND A.DATETIME > B.DATETIME
 ORDER BY
         A.DATETIME;
