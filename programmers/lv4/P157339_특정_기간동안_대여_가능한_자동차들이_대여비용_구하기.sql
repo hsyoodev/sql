451e8b75f118cc67443de86c4a47d301237d84f1
hsyoo <hsyoodev@icloud.com>
2026-02-01 20:51:24 +0900

    retried P157339_특정_기간동안_대여_가능한_자동차들이_대여비용_구하기

--- programmers/lv4/P157339_특정_기간동안_대여_가능한_자동차들이_대여비용_구하기.sql
+++ programmers/lv4/P157339_특정_기간동안_대여_가능한_자동차들이_대여비용_구하기.sql
@@ -1,26 +1,38 @@
+-- 코드를 입력하세요
+WITH
+    -- 2022년 11월 1일 부터 2022년 11월 30일까지 대여불가능한 자동차
+    CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY AS
+    (
+        SELECT
+                DISTINCT CAR_ID
+        FROM
+                CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        WHERE
+                TO_CHAR(START_DATE, 'YYYY-MM-DD') <= '2022-11-30'
+          AND
+                TO_CHAR(END_DATE, 'YYYY-MM-DD') >= '2022-11-01'
+    )
 SELECT
-        A.CAR_ID
-      , A.CAR_TYPE
-      , 30 * (A.DAILY_FEE * (1 - (B.DISCOUNT_RATE * 0.01))) AS FEE
+        CAR_ID
+      , CAR_TYPE
+      , (30 * DAILY_FEE * (1 - (DISCOUNT_RATE * 0.01))) AS FEE
 FROM
-        CAR_RENTAL_COMPANY_CAR A
-      , CAR_RENTAL_COMPANY_DISCOUNT_PLAN B
+        CAR_RENTAL_COMPANY_CAR
+INNER JOIN
+        CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+        USING(CAR_TYPE)
 WHERE
-        A.CAR_TYPE = B.CAR_TYPE
-AND
-        A.CAR_TYPE IN ('세단', 'SUV')
-AND
-        B.DURATION_TYPE = '30일 이상'
-AND
-        A.CAR_ID NOT IN (
+        CAR_ID NOT IN (
                             SELECT
-                                    DISTINCT CAR_ID
+                                    CAR_ID
                             FROM
-                                    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                            WHERE
-                                    TO_CHAR(END_DATE, 'YYYYMMDD') >= '20221101'
-                        )
+                                    CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                      )
 AND
-        30 * (A.DAILY_FEE * (1 - (B.DISCOUNT_RATE * 0.01))) BETWEEN 500000 AND 2000000
+        CAR_TYPE IN ('세단', 'SUV')
+AND
+        DURATION_TYPE = '30일 이상'
+AND
+        (30 * DAILY_FEE * (1 - (DISCOUNT_RATE * 0.01))) BETWEEN 500000 AND 2000000
 ORDER BY
         FEE DESC, CAR_TYPE, CAR_ID DESC;
