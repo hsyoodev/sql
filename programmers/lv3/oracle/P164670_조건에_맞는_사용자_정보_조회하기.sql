6bc62475d174f6937365479751e4e6d7db3a3157
hsyoo <hsyoodev@icloud.com>
2026-02-04 22:09:39 +0900

    retried P164670_조건에_맞는_사용자_정보_조회하기

--- programmers/lv3/oracle/P164670_조건에_맞는_사용자_정보_조회하기.sql
+++ programmers/lv3/oracle/P164670_조건에_맞는_사용자_정보_조회하기.sql
@@ -1,8 +1,9 @@
+-- 코드를 입력하세요
 SELECT
         USER_ID
       , NICKNAME
-      , CITY || ' ' || (STREET_ADDRESS1 || ' ' || STREET_ADDRESS2) AS "전체주소"
-      , (SUBSTR(TLNO, 0, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8, 4)) AS "전화번호"
+      , (CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2) AS "전체주소"
+      , REGEXP_REPLACE(TLNO, '([0-9]{3})([0-9]{4})([0-9]{4})', '\1-\2-\3') AS "전화번호"
 FROM
         USED_GOODS_USER
 WHERE
