df6706f7aa7919220fc2c68753b1243289c73832
hsyoo <hsyoodev@icloud.com>
2026-02-07 21:30:52 +0900

    retried P157340_자동차_대여_기록에서_대여중_대여_가능_여부_구분하기

--- programmers/lv3/oracle/P157340_자동차_대여_기록에서_대여중_대여_가능_여부_구분하기.sql
+++ programmers/lv3/oracle/P157340_자동차_대여_기록에서_대여중_대여_가능_여부_구분하기.sql
@@ -1,15 +1,25 @@
+-- 코드를 입력하세요
+WITH
+    -- 2022년 10월 16일에 대여 중인 자동차
+    CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY AS
+        (
+            SELECT
+                    CAR_ID
+            FROM
+                    CAR_RENTAL_COMPANY_RENTAL_HISTORY
+            WHERE
+                    TO_DATE('2022-10-16', 'YYYY-MM-DD') BETWEEN START_DATE AND END_DATE
+        )
 SELECT
         CAR_ID
       , CASE
             WHEN CAR_ID IN (
-                SELECT
-                        CAR_ID
-                FROM
-                        CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                WHERE
-                        TO_DATE('2022-10-16', 'YYYY-MM-DD HH24:MI:SS') BETWEEN START_DATE AND END_DATE
-            ) THEN '대여중'
-            ELSE '대여 가능'
+                                SELECT
+                                    CAR_ID
+                                FROM
+                                    CUSTOM_CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                           ) THEN '대여중'
+           ELSE '대여 가능'
         END AS AVAILABILITY
 FROM
         CAR_RENTAL_COMPANY_RENTAL_HISTORY
@@ -17,3 +27,4 @@ GROUP BY
         CAR_ID
 ORDER BY
         CAR_ID DESC;
+        
