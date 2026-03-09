78d1eb7579b7cbbf664e27699b65fc8573197529
hsyoo <hsyoodev@icloud.com>
2026-03-09 21:12:01 +0900

    solved AB_테스트를_위한_버킷_나누기_1

---
+++ solvesql/mysql/lv1/AB_테스트를_위한_버킷_나누기_1.sql
@@ -0,0 +1,10 @@
+SELECT
+        DISTINCT customer_id
+      , CASE
+            WHEN customer_id % 10 = 0 THEN 'A'
+            ELSE 'B'
+        END AS bucket
+FROM
+        transactions
+ORDER BY
+        customer_id;
