c1181c5b387cfd585eba7c89ad1a63cd09c135b3
hsyoo <hsyoodev@icloud.com>
2026-04-07 22:36:33 +0900

    solved 레스토랑의_요일별_VIP

---
+++ solvesql/mysql/lv2/레스토랑의_요일별_VIP.sql
@@ -0,0 +1,14 @@
+SELECT
+        *
+FROM
+        tips
+WHERE
+        (day, total_bill) IN (
+                                SELECT
+                                        day
+                                      , MAX(total_bill) AS total_bill
+                                FROM
+                                        tips
+                                GROUP BY
+                                        day
+                             );
