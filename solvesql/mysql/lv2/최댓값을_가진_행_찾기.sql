ab1546df06c8178059f93b979fff3267eecc0f6b
hsyoo <hsyoodev@icloud.com>
2026-04-17 20:32:58 +0900

    solved 최댓값을_가진_행_찾기

---
+++ solvesql/mysql/lv2/최댓값을_가진_행_찾기.sql
@@ -0,0 +1,20 @@
+SELECT
+        id
+FROM
+        points
+WHERE
+        x IN (
+            SELECT
+                MAX(x)
+            FROM
+                points
+        )
+OR
+        y IN (
+            SELECT
+                MAX(y)
+            FROM
+                points
+        )
+ORDER BY
+        id;
