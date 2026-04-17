7c324e51bdbba54d6991e95a4b379789eb87ae6d
hsyoo <hsyoodev@icloud.com>
2026-04-17 20:34:02 +0900

    solved 최댓값을_가진_행_찾기

--- solvesql/mysql/lv2/최댓값을_가진_행_찾기.sql
+++ solvesql/mysql/lv2/최댓값을_가진_행_찾기.sql
@@ -4,17 +4,17 @@ FROM
         points
 WHERE
         x IN (
-            SELECT
-                MAX(x)
-            FROM
-                points
-        )
+                SELECT
+                        MAX(x)
+                FROM
+                        points
+             )
 OR
         y IN (
-            SELECT
-                MAX(y)
-            FROM
-                points
-        )
+                SELECT
+                        MAX(y)
+                FROM
+                        points
+             )
 ORDER BY
         id;
